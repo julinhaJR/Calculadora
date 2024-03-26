d141df727339d1c3bebe73968ea2ddfed1751428
julinhaJR <juliarapozo18@gmail.com>
2024-03-26 20:19:47 -0300

    first commit

---
+++ Calculadora.java
@@ -0,0 +1,8 @@
+public class Calculadora {
+    public double somar (double a, double b){
+        return a + b;
+    }
+    public double substrair (double a, double b){
+        return a - b;    
+    }
+}
